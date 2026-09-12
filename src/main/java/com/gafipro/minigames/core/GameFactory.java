e31c7076553910110dfbe3f1061ce29010677a3e
GafiPro <gabrielriosguedes@gmail.com>
2026-09-12 21:50:44 +0100

    Route chess through dedicated rules engine

--- src/main/java/com/gafipro/minigames/core/GameFactory.java
+++ src/main/java/com/gafipro/minigames/core/GameFactory.java
@@ -3,6 +3,7 @@ package com.gafipro.minigames.core;
 import com.gafipro.minigames.game.Game;
 import com.gafipro.minigames.game.arcade.ArcadeCollection;
 import com.gafipro.minigames.game.board.BoardCollection;
+import com.gafipro.minigames.game.board.ChessGame;
 import com.gafipro.minigames.game.board.ConnectFourGame;
 import com.gafipro.minigames.game.board.RockPaperScissorsGame;
 import com.gafipro.minigames.game.board.TicTacToeGame;
@@ -46,7 +47,7 @@ public final class GameFactory {
             case "connect_four" -> new ConnectFourGame();
             case "rock_paper_scissors" -> new RockPaperScissorsGame();
             case "battleship" -> new BoardCollection.Battleship();
-            case "chess" -> new BoardCollection.Chess();
+            case "chess" -> new ChessGame();
             case "checkers" -> new BoardCollection.Checkers();
             case "dots_and_boxes" -> new BoardCollection.DotsAndBoxes();
             case "four_in_row_mini" -> new BoardCollection.FourInRowMini();
