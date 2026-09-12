bc69bf397de3a9cf731db6a565d55a2c72921908
GafiPro <gabrielriosguedes@gmail.com>
2026-09-12 21:37:27 +0100

    Initialize multiplayer and add games invite command

--- src/main/java/com/gafipro/minigames/GafiMinigamesClient.java
+++ src/main/java/com/gafipro/minigames/GafiMinigamesClient.java
@@ -1,6 +1,8 @@
 package com.gafipro.minigames;
 
 import com.gafipro.minigames.gui.GamesScreen;
+import com.gafipro.minigames.multiplayer.MultiplayerManager;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -8,15 +10,15 @@ import net.minecraft.client.MinecraftClient;
 
 public final class GafiMinigamesClient implements ClientModInitializer {
     public static final String MOD_ID = "gafi-minigames";
-
-    @Override
-    public void onInitializeClient() {
+    @Override public void onInitializeClient() {
+        MultiplayerManager.init();
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
             dispatcher.register(ClientCommandManager.literal("games")
-                .executes(context -> {
-                    MinecraftClient.getInstance().setScreen(new GamesScreen(null));
-                    return 1;
-                }))
+                .executes(context -> { MinecraftClient.getInstance().setScreen(new GamesScreen(null)); return 1; })
+                .then(ClientCommandManager.literal("invite")
+                    .then(ClientCommandManager.argument("player", StringArgumentType.word())
+                        .executes(context -> { MultiplayerManager.invite(StringArgumentType.getString(context,"player"), "connect_four"); return 1; })))
+            )
         );
     }
 }
