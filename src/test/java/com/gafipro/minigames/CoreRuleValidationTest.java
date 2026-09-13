b64090b39278b19257a6bc427987c647891870b4
GafiPro <gabrielriosguedes@gmail.com>
2026-09-13 10:40:27 +0100

    Strengthen core arcade and 2048 rule tests

--- src/test/java/com/gafipro/minigames/CoreRuleValidationTest.java
+++ src/test/java/com/gafipro/minigames/CoreRuleValidationTest.java
@@ -5,6 +5,7 @@ import com.gafipro.minigames.core.GameFactory;
 import com.gafipro.minigames.core.WordBank;
 import com.gafipro.minigames.game.Game;
 import com.gafipro.minigames.game.arcade.ArcadeCollection;
+import com.gafipro.minigames.game.arcade.WhackAMoleGame;
 import com.gafipro.minigames.game.board.ConnectFourGame;
 import com.gafipro.minigames.game.puzzle.Game2048;
 import org.junit.jupiter.api.DynamicTest;
@@ -36,10 +37,14 @@ class CoreRuleValidationTest {
     @Test void catalogAndFactoryIdsAreExactlyOneToOne(){assertEquals(new HashSet<>(GameCatalog.all().stream().map(GameCatalog.Entry::id).collect(Collectors.toSet())),new HashSet<>(GameFactory.supportedIds()));}
     @Test void factoryRejectsUnknownIds(){assertThrows(IllegalArgumentException.class,()->GameFactory.create("definitely_not_a_game"));}
 
-    @Test void twentyFortyEightDoesNotMergeAResultTwice()throws Exception{Game2048 game=new Game2048();Method m=Game2048.class.getDeclaredMethod("mergeLine",int[].class);m.setAccessible(true);assertArrayEquals(new int[]{4,4,0,0},(int[])m.invoke(game,(Object)new int[]{2,2,2,2}));}
-    @Test void twentyFortyEightOnlyMergesAdjacentCompactedPairs()throws Exception{Game2048 game=new Game2048();Method m=Game2048.class.getDeclaredMethod("mergeLine",int[].class);m.setAccessible(true);assertArrayEquals(new int[]{4,2,0,0},(int[])m.invoke(game,(Object)new int[]{2,0,2,2}));}
+    private static int[] merge(Game2048 game,int[] line)throws Exception{Method m=Game2048.class.getDeclaredMethod("mergeLine",int[].class);m.setAccessible(true);return (int[])m.invoke(game,(Object)line);}
+    @Test void twentyFortyEightDoesNotMergeAResultTwice()throws Exception{assertArrayEquals(new int[]{4,4,0,0},merge(new Game2048(),new int[]{2,2,2,2}));}
+    @Test void twentyFortyEightOnlyMergesAdjacentCompactedPairs()throws Exception{assertArrayEquals(new int[]{4,2,0,0},merge(new Game2048(),new int[]{2,0,2,2}));}
+    @Test void twentyFortyEightMergesLongRunsCorrectly()throws Exception{assertArrayEquals(new int[]{8,4,0,0},merge(new Game2048(),new int[]{4,4,4,0}));assertArrayEquals(new int[]{16,16,0,0},merge(new Game2048(),new int[]{8,8,16,0}));}
+
     @Test void connectFourDetectsDiagonalWins()throws Exception{Method m=ConnectFourGame.class.getDeclaredMethod("hasWon",int[][].class,int.class);m.setAccessible(true);int[][]b={{0,0,0,1,0,0,0},{0,0,1,2,0,0,0},{0,1,2,2,0,0,0},{1,2,2,1,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0}};assertTrue((boolean)m.invoke(null,(Object)b,1));}
     @Test void connectFourDoesNotAcceptAThreeInARow()throws Exception{Method m=ConnectFourGame.class.getDeclaredMethod("hasWon",int[][].class,int.class);m.setAccessible(true);int[][]b={{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,1,0,0,0},{0,0,1,1,0,0,0},{0,0,0,0,0,0,0}};assertFalse((boolean)m.invoke(null,(Object)b,1));}
     @Test void wordBankIsSubstantialAndSanitized(){assertTrue(WordBank.all().size()>=64);assertTrue(WordBank.all().stream().allMatch(word->word.matches("[A-Z]{3,18}")));}
     @Test void fastClickRequiresAllTwentyTargets()throws Exception{Method m=ArcadeCollection.FastClick.class.getDeclaredMethod("completed",int.class);m.setAccessible(true);assertFalse((boolean)m.invoke(null,19));assertTrue((boolean)m.invoke(null,20));assertTrue((boolean)m.invoke(null,25));}
+    @Test void whackAMoleNeedsAtLeastOneRealHitToWin()throws Exception{Method m=WhackAMoleGame.class.getDeclaredMethod("completed",int.class);m.setAccessible(true);assertFalse((boolean)m.invoke(null,0));assertTrue((boolean)m.invoke(null,1));}
 }
